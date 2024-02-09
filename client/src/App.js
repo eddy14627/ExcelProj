e18a5aef26b00870e2819ddba5d877c9b53cfe00
Adarsh Mishra <adjee2020@gmail.com>
2024-02-10 03:11:16 +0530

    third

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css"; // Import the CSS file
 
-function App() {
+const FileUploader = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadStatus, setUploadStatus] = useState(null);
+
+  const handleFileChange = (event) => {
+    const newFile = event.target.files[0];
+
+    if (newFile || selectedFile) {
+      setSelectedFile(newFile);
+      setUploadStatus(null);
+
+      event.target.value = null;
+    }
+  };
+
+  const handleUpload = async () => {
+    if (!selectedFile) {
+      alert("Please select a file before uploading.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/importUser",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log("File uploaded successfully:", response.data);
+      setUploadStatus("success");
+      setSelectedFile(null);
+    } catch (error) {
+      console.error("Error uploading file:", error.message);
+      setSelectedFile(null);
+      setUploadStatus("error");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <label className="fileInputLabel">
+        Choose File
+        <input
+          type="file"
+          onChange={handleFileChange}
+          className="fileInput"
+          cursor="pointer"
+        />
+      </label>
+      <button onClick={handleUpload} className="uploadButton">
+        Upload File
+      </button>
+
+      {uploadStatus === "success" && (
+        <p className="successMessage">File uploaded successfully!</p>
+      )}
+
+      {uploadStatus === "error" && (
+        <p className="errorMessage">Something went wrong. Please try again.</p>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default FileUploader;
