7e6be0ca417df8a5e09813f17357cb33ecc7a77f
Adarsh Mishra <adjee2020@gmail.com>
2024-02-11 12:56:07 +0530

    ll

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 const FileUploader = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [uploadStatus, setUploadStatus] = useState(null);
-  const [tableData, setTableData] = useState([]);
-
-  const fetchDataFromServer = async () => {
-    try {
-      const response = await axios.get(
-        "https://excelproj.onrender.com/getUserData"
-      );
-      setTableData(response.data);
-    } catch (error) {
-      console.error("Error fetching data from server:", error.message);
-    }
-  };
 
   const handleFileChange = (event) => {
     const newFile = event.target.files[0];
@@ -55,10 +43,6 @@ const FileUploader = () => {
     }
   };
 
-  useEffect(() => {
-    fetchDataFromServer();
-  }, [uploadStatus]);
-
   return (
     <div className="container">
       <label className="fileInputLabel">
@@ -80,29 +64,6 @@ const FileUploader = () => {
 
       {uploadStatus === "error" && (
         <p className="errorMessage">Something went wrong. Please try again.</p>
-      )}
-
-      {tableData.length > 0 && (
-        <table className="dataTable">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Nationality</th>
-              <th>City</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableData.map((data) => (
-              <tr key={data.id}>
-                <td>{data.id}</td>
-                <td>{data.name}</td>
-                <td>{data.nationality}</td>
-                <td>{data.city}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
       )}
     </div>
   );
